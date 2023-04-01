721e2f605cf2e40c617339f4c7a553f59d64b5d9
Weronika Piotrowska <weronika.piotrowska.pl@gmail.com>
2023-04-01 14:15:50 +0200

    Update run_jmeter_script.sh

--- run_jmeter_script.sh
+++ run_jmeter_script.sh
@@ -14,7 +14,7 @@ for iteration in `seq 1 $iterations`; do
 	mkdir $iteration
 	
 	for user in `seq 1 $users`; do
-		eval docker run -v /home/ec2-user/:/mnt/jmeter/ justb4/jmeter:latest -n -t /mnt/jmeter/Pi2_parametrised.jmx -l log.jtl -Juser=$user -Jiteration=$iteration -Jaddress=$address
+		eval docker run -v /home/ec2-user/:/mnt/jmeter/ justb4/jmeter:latest -n -t /mnt/jmeter/Pi2_parametrised.jmx -l log.jtl -Juser=$user -Jiteration=$iteration -Jaddress=$address -l /mnt/jmeter/
 		sleep $sleep
 	done;
 done;
